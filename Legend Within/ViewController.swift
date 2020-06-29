d638fd2194cea269c0f25d52e63f8ca372c61cf7
Balazs Erdesz <balazs.erdesz@logmein.com>
2020-06-29 15:53:02 +0200

    Initial Commit

---
+++ Legend Within/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Legend Within
+//
+//  Created by Balazs Erdesz on 2020. 06. 29..
+//  Copyright © 2020. EBUniApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
